cd1e7855cd40d438d3a1ec9e3b164651025d3b4d
C3NZ <cenz@cenz.io>
2019-01-24 14:14:00 -0800

    [feat] implement two-fer

---
+++ two-fer/two_fer.go
@@ -0,0 +1,10 @@
+package twofer
+
+func ShareWith(name string) string {
+    // Determine who's going to be sharing with me
+    // Return - A string formatted properly to who's sharing with me
+    if len(name) == 0 {
+        name = "you"
+    }
+	return "One for " + name + ", one for me."
+}
