b5591195df21415bb0489fb42f56edd67dcfdee9
C3NZ <cenz@cenz.io>
2019-02-05 11:23:30 -0800

    [feat] complete the exercism gigasecond challenge

---
+++ gigasecond/gigasecond.go
@@ -0,0 +1,13 @@
+// Calculate the moment when someone had lived for a gigasecond
+package gigasecond
+
+// import path for the time package from the standard library
+import "time"
+
+// Calculate the moment someone has lived for a gigasecond
+// Given a starting date
+func AddGigasecond(t time.Time) time.Time {
+    duration := time.Duration(1000000000000000000)
+    t = t.Add(duration)
+	return t
+}
