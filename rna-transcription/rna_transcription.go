4cd036e8ebcbf6601fa15cc049c0f78a3f844666
C3NZ <cenz@cenz.io>
2019-02-20 22:48:15 -0800

    [feat] document code

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,6 +1,7 @@
 package strand
 
 func ToRNA(dna string) string {
+    // Create RNA mapping
     RNAComplement := map[rune]rune{
         'G':'C',
         'C':'G',
@@ -8,10 +9,14 @@ func ToRNA(dna string) string {
         'A':'U',
     }
 
+    // Create output RNA rune slice
     outputRNA := make([]rune, len(dna))
+
+    // Map DNA to RNA
     for pos, char := range dna {
         outputRNA[pos] = RNAComplement[char]
     }
-    
+   
+    // Return the string of the outputRNA rune slice
     return string(outputRNA)
 }
